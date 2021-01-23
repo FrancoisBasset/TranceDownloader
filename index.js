6a52632e8eb114f306451b2a7a1e9dddcc691319
FrancoisBasset <>
2021-01-23 22:23:08 +0100

    scroll

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+app.listen(3000, function() {
+	console.log('Start on 3000');
+});
+
+app.use(express.static('./public', {
+	index: 'scroll1.html'
+}));
