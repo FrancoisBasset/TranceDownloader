56ff218f593792c55d37c061070333c7450e95eb
FrancoisBasset <>
2021-01-25 20:16:04 +0100

    refacto et modifier

--- public/scroll3.js
+++ public/scroll3.js
@@ -1,31 +1,43 @@
 var wishesTable = document.getElementById('wishesTable').getElementsByTagName('tbody')[0];
-var artistInput = document.getElementById('artistInput');
-var trackInput = document.getElementById('trackInput');
+var artistInputAdd = document.getElementById('artistInputAdd');
+var trackInputAdd = document.getElementById('trackInputAdd');
+var wishesSelect = document.getElementById('wishesSelect');
+var artistInputUpdate = document.getElementById('artistInputUpdate');
+var trackInputUpdate = document.getElementById('trackInputUpdate');
 
 fetch('/wishes').then(function(response) {
 	response.json().then(function(json) {
 		for (const wish of json.response) {
 			if (!wish.done) {
-				addRow(wish.id, wish.artist, wish.track, wish.done);
+				addRow(wish.id, wish.artist, wish.track);
 			}
+
+			addOption(wish.id, wish.artist, wish.track, wish.done);
 		}
 	});
 });
 
-function addRow(id, artist, track, done) {
+function addRow(id, artist, track) {
 	const row = wishesTable.insertRow();
 	row.id = 'wish-' + id;
 	row.insertCell().innerText = artist;
 	row.insertCell().innerText = track;
-	
-	if (done) {
-		row.insertCell().innerHTML = '<input type="checkbox" onclick="return false" checked />';
-	} else {
-		row.insertCell().innerHTML = '<input type="checkbox" onclick="return false" />';
-	}
-
 	row.insertCell().innerHTML = `<button onclick="searchWish('${artist}', '${track}')">Rechercher</button>`;
 	row.insertCell().innerHTML = `<button onclick="doneWish(${id})">Done</button>`;
+}
+
+function addOption(id, artist, track, done) {
+	wishesSelect.add(
+		new Option(
+			artist + ' ' + track,
+			JSON.stringify({
+				id: id,
+				artist: artist,
+				track: track,
+				done: done
+			})
+		)
+	);
 }
 
 function searchWish(artist, track) {
@@ -48,15 +60,42 @@ function addWish() {
 			'Content-Type': 'application/json'
 		},
 		body: JSON.stringify({
-			artist: artistInput.value,
-			track: trackInput.value
+			artist: artistInputAdd.value,
+			track: trackInputAdd.value
 		})
 	}).then(function(response) {
 		response.json().then(function(json) {
-			artistInput.value = '';
-			trackInput.value = '';
+			artistInputAdd.value = '';
+			trackInputAdd.value = '';
 
-			addRow(json.response.id, json.response.artist, json.response.track, json.response.done);
+			addRow(json.response.id, json.response.artist, json.response.track);
+		});
+	});
+}
+
+function updateForm() {
+	const wish = JSON.parse(wishesSelect.value);
+
+	artistInputUpdate.value = wish.artist;
+	trackInputUpdate.value = wish.track;
+}
+
+function updateWish() {
+	const wish = JSON.parse(wishesSelect.value);
+
+	fetch('/wishes/' + wish.id, {
+		method: 'PUT',
+		headers: {
+			'Accept': 'application/json',
+			'Content-Type': 'application/json'
+		},
+		body: JSON.stringify({
+			artist: artistInputUpdate.value,
+			track: trackInputUpdate.value
+		})
+	}).then(function(response) {
+		response.json().then(function(json) {
+
 		});
 	});
 }
