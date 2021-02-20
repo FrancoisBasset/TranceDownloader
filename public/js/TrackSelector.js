88c326b1cb395e1595e900284f57923872ee1604
FrancoisBasset <>
2021-02-20 21:08:09 +0100

    track selector

---
+++ public/js/TrackSelector.js
@@ -0,0 +1,105 @@
+class TrackSelector {
+	#backButton = document.getElementById('backButton');
+	#headLabel = document.getElementById('headLabel');
+	#genresDiv = document.getElementById('genresDiv');
+	#genres = []
+	#currentGenre = null
+	#currentArtist = null
+	#currentTrack = null
+
+	constructor(genres) {
+		this.#genres = genres;
+
+		this.#showGenres();
+	}
+
+	#showGenres() {
+		this.#backButton.style.display = 'none';
+
+		this.#headLabel.innerText = 'Genres';
+		this.#headLabel.style.width = '100%';
+		this.#headLabel.style.backgroundColor = this.#getRandomColor();
+		this.#genresDiv.innerHTML = '';
+	
+		for (const genre of Object.keys(genres)) {
+			const genreDiv = document.createElement('div');
+			genreDiv.innerText = genre;
+			genreDiv.style.padding = '20px';
+			genreDiv.style.border = '1px solid black';
+			genreDiv.style.color = 'white';
+			genreDiv.style.backgroundColor = this.#getRandomColor();
+	
+			genreDiv.onclick = () => {
+				this.#currentGenre = genreDiv.innerText;
+				this.#showArtists();
+			}
+	
+			this.#genresDiv.appendChild(genreDiv);
+		}
+	}
+
+	#showArtists() {
+		this.#backButton.style.display = 'inline';
+		this.#backButton.style.backgroundColor = this.#getRandomColor();
+		this.#backButton.onclick = () => {
+			this.#showGenres();
+		}
+		this.#headLabel.innerText = this.#currentGenre;
+		this.#genresDiv.innerHTML = '';
+	
+		for (const artist of Object.keys(genres[this.#currentGenre])) {
+			const artistDiv = document.createElement('div');
+			artistDiv.innerText = artist;
+			artistDiv.style.padding = '20px';
+			artistDiv.style.border = '1px solid black';
+			artistDiv.style.color = 'white';
+			artistDiv.style.backgroundColor = this.#getRandomColor();
+	
+			artistDiv.onclick = () => {
+				this.#currentArtist = artistDiv.innerText;
+				this.#showTracks();
+			}
+	
+			this.#genresDiv.appendChild(artistDiv);
+		}
+	}
+
+	#showTracks() {
+		this.#backButton.style.display = 'inline';
+		this.#backButton.onclick = () => {
+			this.#showArtists();
+		}
+		this.#headLabel.innerText = this.#currentGenre + " > " + this.#currentArtist;
+		this.#genresDiv.innerHTML = '';
+	
+		for (const track of genres[this.#currentGenre][this.#currentArtist]) {
+			const trackDiv = document.createElement('div');
+			trackDiv.innerText = track.title;
+			trackDiv.style.padding = '20px';
+			trackDiv.style.border = '1px solid black';
+			trackDiv.style.color = 'white';
+			trackDiv.style.backgroundColor = this.#getRandomColor();
+	
+			trackDiv.onclick = () => {
+				this.#currentTrack = track;
+				
+				const audio = document.createElement('audio');
+				audio.src = this.#currentTrack.url;
+				audio.controls = true;
+				audio.className = 'scroll';
+	
+				this.#genresDiv.appendChild(audio);
+			}
+	
+			this.#genresDiv.appendChild(trackDiv);
+		}
+	}
+
+	#getRandomColor() {
+		const r = Math.floor(Math.random() * 75);
+		const g = Math.floor(Math.random() * 75);
+		const b = Math.floor(Math.random() * 75);
+	
+		return `rgb(${r}, ${g}, ${b})`;
+	}
+}
--- public/js/scroll1.js
+++ public/js/scroll1.js
@@ -1,120 +1,26 @@
-var genresList = new GenresList();
-var step = 'genre';
-
-var backButton = document.getElementById('backButton');
-var headLabel = document.getElementById('headLabel');
-var genresDiv = document.getElementById('genresDiv');
-var genres = [];
-var currentGenre = null;
-var currentArtist = null;
-var track = null;
+var trackSelector = null;
 
 fetch('/library').then(function(response) {
 	response.json().then(function(json) {
-		const tracks = json.response;
-
-		for (const track of tracks) {
-			if (genres[track.genre] == null) {
-				genres[track.genre] = [];
-			}
-
-			if (genres[track.genre][track.artist] == null) {
-				genres[track.genre][track.artist] = [];
-			}
-
-			genres[track.genre][track.artist].push(track);
-		}
-
-		showGenres();
+		genres = buildGenres(json.response);
+		trackSelector = new TrackSelector(genres);
 	});
 });
 
-function showGenres() {
-	backButton.style.display = 'none';
+function buildGenres(tracks) {
+	const genres = [];
 
-	headLabel.innerText = 'Genres';
-	headLabel.style.width = '100%';
-	headLabel.style.backgroundColor = getRandomColor();
-	genresDiv.innerHTML = '';
-
-	for (const genre of Object.keys(genres)) {
-		const genreDiv = document.createElement('div');
-		genreDiv.innerText = genre;
-		genreDiv.style.padding = '20px';
-		genreDiv.style.border = '1px solid black';
-		genreDiv.style.color = 'white';
-		genreDiv.style.backgroundColor = getRandomColor();
-
-		genreDiv.onclick = function() {
-			currentGenre = genreDiv.innerText;
-			showArtists();
+	for (const track of tracks) {
+		if (genres[track.genre] == null) {
+			genres[track.genre] = [];
 		}
 
-		genresDiv.appendChild(genreDiv);
-	}
-}
-
-function showArtists() {
-	backButton.style.display = 'inline';
-	backButton.style.backgroundColor = getRandomColor();
-	backButton.onclick = function() {
-		showGenres();
-	}
-	headLabel.innerText = currentGenre;
-	genresDiv.innerHTML = '';
-
-	for (const artist of Object.keys(genres[currentGenre])) {
-		const artistDiv = document.createElement('div');
-		artistDiv.innerText = artist;
-		artistDiv.style.padding = '20px';
-		artistDiv.style.border = '1px solid black';
-		artistDiv.style.color = 'white';
-		artistDiv.style.backgroundColor = getRandomColor();
-
-		artistDiv.onclick = function() {
-			currentArtist = artistDiv.innerText;
-			showTracks();
+		if (genres[track.genre][track.artist] == null) {
+			genres[track.genre][track.artist] = [];
 		}
 
-		genresDiv.appendChild(artistDiv);
+		genres[track.genre][track.artist].push(track);
 	}
-}
 
-function showTracks() {
-	backButton.style.display = 'inline';
-	backButton.onclick = function() {
-		showArtists();
-	}
-	headLabel.innerText = currentGenre + " > " + currentArtist;
-	genresDiv.innerHTML = '';
-
-	for (const track of genres[currentGenre][currentArtist]) {
-		const trackDiv = document.createElement('div');
-		trackDiv.innerText = track.title;
-		trackDiv.style.padding = '20px';
-		trackDiv.style.border = '1px solid black';
-		trackDiv.style.color = 'white';
-		trackDiv.style.backgroundColor = getRandomColor();
-
-		trackDiv.onclick = function() {
-			currentTrack = track;
-			
-			const audio = document.createElement('audio');
-			audio.src = currentTrack.url;
-			audio.controls = true;
-			audio.className = 'scroll';
-
-			genresDiv.appendChild(audio);
-		}
-
-		genresDiv.appendChild(trackDiv);
-	}
-}
-
-function getRandomColor() {
-	const r = Math.floor(Math.random() * 75);
-	const g = Math.floor(Math.random() * 75);
-	const b = Math.floor(Math.random() * 75);
-
-	return `rgb(${r}, ${g}, ${b})`;
+	return genres;
 }
