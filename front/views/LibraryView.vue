5ae8cf291ba6292f8bcda2a90de486f650f93eda
FrancoisBasset <mrfrancoisbasset@gmail.com>
2024-12-25 13:24:46 +0100

    refactor(library): remove dead code

--- front/views/LibraryView.vue
+++ front/views/LibraryView.vue
@@ -5,45 +5,9 @@
 		<div class="bg-sky-100 !overflow-scroll rounded-lg h-[80vh]">
 			<TrackList  />
 		</div>
-		<!--<DefinitionsForm id="definitions-form" v-if="library.track" :track="library.track" :key="library.track" />-->
 	</div>
 </template>
 
-<style scoped>
-#track-selector {
-	position: absolute;
-	left: 5%;
-	top: 15%;
-}
-
-#definitions-form {
-	position: absolute;
-	left: 50%;
-	top: 15%;
-}
-
-#music-player {
-	position: absolute;
-	width: 100%;
-	bottom: 0;
-}
-</style>
-
 <script setup>
 import TrackList from '@/components/TrackList.vue';
-//import DefinitionsForm from '@/components/DefinitionsForm.vue';
-import useLibraryStore from '@/stores/Library';
-</script>
-
-<script>
-export default {
-	data() {
-		return {
-			library: useLibraryStore()
-		};
-	},
-	created() {
-		this.library.initTracks();
-	}
-};
 </script>
